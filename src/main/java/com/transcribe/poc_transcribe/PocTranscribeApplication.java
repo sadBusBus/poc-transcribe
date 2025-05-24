a16b3fc6a07f38de962f01a90df787b88f902f60
BusBus <guesanjo.ibkr@gmail.com>
2025-05-24 12:09:14 +0200

    feat: scaffold

---
+++ src/main/java/com/transcribe/poc_transcribe/PocTranscribeApplication.java
@@ -0,0 +1,13 @@
+package com.transcribe.poc_transcribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocTranscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocTranscribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transcribe/poc_transcribe/PocTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcribe.poc_transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
